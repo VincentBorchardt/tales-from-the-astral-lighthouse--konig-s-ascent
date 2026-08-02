80252bb30e2807966881b4d3e91955943697ea51
VincentBorchardt <vborchardt123@gmail.com>
2026-08-01 19:14:54 -0500

    First part of final level done
    Starting part of the final cutscene is in, and lots of putting things together are in as well

--- Scripts/Gameplay/final_level.gd
+++ Scripts/Gameplay/final_level.gd
@@ -11,6 +11,7 @@ enum EndingChoice {GOOD, BAD, DSD}
 @export var trik: Array[Message]
 @export var primeval_mask: Array[Message]
 @export var pitmaster: Array[Message]
+@export var pitmaster_alt: Array[Message]
 
 @export_group("Good Ending Conversations")
 @export var good_ending_count: int = 10
@@ -24,8 +25,11 @@ enum EndingChoice {GOOD, BAD, DSD}
 @export var bad: Array[Message]
 @export var dsd_part_1: Array[Message]
 @export var dsd_part_2: Array[Message]
+@export var dsd_part_3: Array[Message]
 
-var initial_total_convo: Array[Message] = initial_part_1
+@onready var initial_total_convo: Array[Message] = initial_part_1
+var second_total_convo: Array[Message] = []
+var cameos_total_convo: Array[Message] = []
 var chosen_ending: EndingChoice
 
 # TODO if I refactor tutorial out of general gameplay, we need scripted_progression_count here
@@ -33,20 +37,38 @@ var chosen_ending: EndingChoice
 func set_up_cutscenes():
 	if StoryAutoload.savedDSD():
 		chosen_ending = EndingChoice.DSD
+		initial_total_convo.append_array(dsd_part_1)
+		second_total_convo = dsd_part_2
 	else:
-		pass
+		initial_total_convo.append(initial_part_2)
+		if StoryAutoload.savedMask():
+			cameos_total_convo.append_array(primeval_mask)
+			if StoryAutoload.savedPit():
+				cameos_total_convo.append_array(pitmaster)
+		elif StoryAutoload.savedPit():
+			cameos_total_convo.append_array(pitmaster_alt)
+		if StoryAutoload.savedTrik():
+			cameos_total_convo.append_array(trik)
+		second_total_convo.append_array(cameos_total_convo)
+		second_total_convo.append_array(initial_part_3)
+		if StoryAutoload.total_saved_npcs >= good_ending_count:
+			chosen_ending = EndingChoice.GOOD
+			second_total_convo.append_array(good_part_1)
+		else:
+			chosen_ending = EndingChoice.BAD
+			second_total_convo.append(bad)
 
 func continue_scripted_sequence():
 	match scripted_progression_count:
 		1:
 			match chosen_ending:
-				EndingChoice.GOOD:
-					pass
-				EndingChoice.BAD:
-					pass
 				EndingChoice.DSD:
 					# animate in DSD
 					pass
+				_:
+					# animate in cameos
+					pass
+			start_cutscene(second_total_convo)
 
 func _on_cutscene_overlay_cutscene_ended() -> void:
 	print("ending cutscene")
@@ -56,4 +78,4 @@ func _on_cutscene_overlay_cutscene_ended() -> void:
 func _on_booth_warp_out_finished() -> void:
 	set_up_cutscenes()
 	scripted_progression_count += 1
-	start_cutscene(StoryAutoload.tutorial_5)
+	call_deferred("start_cutscene", initial_total_convo)
