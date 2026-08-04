802a549e7c696b2b5ad77613b22be30800c8b74f
VincentBorchardt <vborchardt123@gmail.com>
2026-08-03 23:47:24 -0500

    Controller screen added to intro
    Works well, simple

--- Scripts/Cutscenes/intro_scene.gd
+++ Scripts/Cutscenes/intro_scene.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @export var messages: Array[Message]
 
+@onready var controller_message = $ControllerMessage
 @onready var logo = $Logo
+@onready var logo_animation = $Logo/AnimationPlayer
 @onready var titles = $Titles
 @onready var titles_animation = $Titles/AnimationPlayer
 @onready var title_screen = $TitleScreen
@@ -10,6 +12,10 @@ extends Node2D
 func start_intro():
 	$CutsceneOverlay.start_cutscene(messages, false)
 
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("advance_text"):
+		controller_message.visible = false
+		logo_animation.play("logo_image")
 
 func _on_cutscene_overlay_cutscene_ended() -> void:
 	titles.show()
